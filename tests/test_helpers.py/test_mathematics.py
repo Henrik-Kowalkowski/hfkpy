bddc4222472e4f149eeccbf4d819e9d6a2c64aed
hkowalkowski <henrikkowalkowski@gmail.com>
2022-04-06 21:09:21 -0500

    note

--- tests/test_helpers.py/test_mathematics.py
+++ tests/test_helpers.py/test_mathematics.py
@@ -1,6 +1,7 @@
 import sys, pathlib
 
 # get the dynamic path to the file so we can edit the tests in interactive mode
+# need to put hfkpy on the python path to get this to work without pathlib
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
 
 from hfkpy.helpers.mathematics import *

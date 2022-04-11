8628888cf3a05519866c64547cccdc3e7e8b7c4d
hkowalkowski <henrikkowalkowski@gmail.com>
2022-04-10 23:02:20 -0500

    v0.1.2

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 An example of a tested and installable package of helper functions.
 
 ## To install from Git
-`pip install git+https://github.com/Henrik-Kowalkowski/hfkpy@v0.1.0`
+`pip install git+https://github.com/Henrik-Kowalkowski/hfkpy@v0.1.2`
+
+## To document
+1. `conda install sphinx` in git bash
+2. `sphinx-quickstart` in git bash
+3. `pip install sphinx-rtd-theme` in git bash
+4. `conda install pandoc` in git bash
+5. `make html` in cmd (if on Windows)
+
+## Git actions
+`python conda_env_export.py > envhfkpy.yml`  -- for developing the package
+`conda conda_env_export.py > test_envhfkpy.yml`  -- for CICD testing purposes
+https://github.com/conda/conda/issues/9628
+https://autobencoder.com/2020-08-24-conda-actions/
--- version_it.py
+++ version_it.py
@@ -2,9 +2,9 @@
 
 quit = "n"
 while str.lower(quit) != "y":
-    version = input("enter your version number here (e.g. x.x.x): ")
-    print(f"are you sure you want to version to {version}?")
-    answer = input("enter y/n: ")
+    version = input("Enter your version number here (e.g. x.x.x): ")
+    print(f"Are you sure you want to version to {version}?")
+    answer = input("Enter y/n: ")
     if str.lower(answer) == "y":
         with open("README_template.md", "r") as f:
             readme = f.read().format(version=version)
@@ -15,7 +15,8 @@ while str.lower(quit) != "y":
         with open("definitions.py", "w") as f:
             f.write(f"version='{version}'")
 
+        print("Don't forget to update the docs via `make html`...")
         quit = "y"
     else:
-        quit = input("exit? y/n: ")
+        quit = input("Exit? y/n: ")
 
